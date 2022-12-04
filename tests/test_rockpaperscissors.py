05f69b5e0e101b14d64354f2c59c777b3b6ac25b
Philip Wong <4522927+p2635@users.noreply.github.com>
2022-12-04 13:45:05 +0000

    day 2 part 1

--- tests/test_rockpaperscissors.py
+++ tests/test_rockpaperscissors.py
@@ -1,12 +1,33 @@
+"""Module providing tests for Advent of Code Day 2."""
+
 # -----------------------------------------------
 # Imports
 # -----------------------------------------------
 
-# from day2 import day2
+import pytest
+from day2.day2 import *
 
 # -----------------------------------------------
 # Tests
 # -----------------------------------------------
 
-def test_basic():
-    assert 1==1
+@pytest.mark.parametrize("gesture", ["A", "X"])
+def test_get_gesture_rock(gesture):
+    assert get_gesture(gesture) == "Rock"
+
+@pytest.mark.parametrize("gesture", ["B", "Y"])
+def test_get_gesture_paper(gesture):
+    assert get_gesture(gesture) == "Paper"
+
+@pytest.mark.parametrize("gesture", ["C", "Z"])
+def test_get_gesture_scissors(gesture):
+    assert get_gesture(gesture) == "Scissors"
+
+gesture_scores = [
+    ("Rock", 1),
+    ("Paper", 2),
+    ("Scissors", 3),
+]
+@pytest.mark.parametrize('gesture, score', gesture_scores)
+def test_get_gesture_score(gesture, score):
+    assert get_gesture_score(gesture) == score
