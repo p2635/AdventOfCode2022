d35585bde5e6aa38aa63108a90c0e05a91608f19
Philip Wong <4522927+p2635@users.noreply.github.com>
2022-12-04 11:18:14 +0000

    add pytest start

---
+++ day2/day2.py
@@ -0,0 +1,20 @@
+# Rock paper scissors scores
+SelectScores = {
+    "Rock":{
+        "Values": "A", "X"
+        "Score": 1
+    },
+    "Paper":{
+        "Values": "B", "Y"
+        "Score": 2
+    },
+    "Scissors":{
+        "Values": "C", "Z"
+        "Score": 3
+    }
+}
+
+# outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
+# How do I implement the rules in code?
+def giveOutcome(elfOne, elfTwo):
+    pass
