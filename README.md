b87d793cac3b4f1e3ca7e102a078cdc92706795a
Philip Wong <4522927+p2635@users.noreply.github.com>
2022-12-05 12:47:24 +0000

    day 4 complete

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Note: I am a python n00b, so this will make my brain work.
 - [x] Day 1: Calorie Counting 🍔
 - [x] Day 2: Rock Paper Scissors 👊
 - [x] Day 3: Rucksack Reorganization 💰
-- [ ] Day 4: Camp Cleanup 🏕️
+- [x] Day 4: Camp Cleanup 🏕️
 - [ ] Day 5
 - [ ] Day 6
 - [ ] Day 7
--- day4/Day4.py
+++ day4/Day4.py
@@ -1,8 +1,10 @@
 ####################################################
-## Day 4 (Part 1) - The camp cleanup event
+## Day 4 - The camp cleanup event
 ####################################################
 
-overlap = 0 # the overlap count
+elf_count = 0
+full_overlap = 0 # the full overlap count
+partial_overlap = 0 # the partial overlap count
 
 # Data handling
 with open("input/d4.txt", encoding="utf-8", mode="r") as file:
@@ -11,6 +13,8 @@ with open("input/d4.txt", encoding="utf-8", mode="r") as file:
 
 # OK OK, all elf pairs line up, let's see who is overlapping here!
 for string in camp_pairs:
+
+    elf_count += 1
 
     ##################################################
     # MAKING THE ELF MACHINE SCANNER WORK
@@ -41,17 +45,24 @@ for string in camp_pairs:
     print(f"Elf 1 has the assigned areas: {elf1}")
     print(f"Elf 2 has the assigned areas: {elf2}")
 
-    # If either party have done the counterpart's work, mark it down.
+    # If either elf completely cleaned the other elf's area, mark it down.
     if elf1.intersection(elf2) == elf2 or elf2.intersection(elf1) == elf1:
-        print("There is overlap:")
-        print(elf1.intersection(elf2))
-        overlap += 1
+        print("There is complete overlap here between the elves.")
+        full_overlap += 1
+        partial_overlap += 1
+    # If either elf partially cleaned the other elf's area, mark it down.
+    elif elf1.intersection(elf2) or elf2.intersection(elf1):
+        print("There is only partial overlap here between the elves.")
+        partial_overlap += 1
     else:
     # Otherwise, do nothing.
         print("There is no overlap.")
 
     # Move on to the next elf pair
-    print("====== NEXT PAIR. ======")
+    print("====== NEXT PAIR. ======\n")
 
 # The conclusion of the event.
-print(f"===== CONCLUSION =====\nThere are {overlap} overlaps for camp cleanup work.")
+print(f"===== CONCLUSION =====\n* There are {elf_count} elves. \
+    \n* Full Overlap: {full_overlap} elves. \
+    \n* Partial and Full Overlap: {partial_overlap} elves. \
+    \n* This is unacceptable.")
