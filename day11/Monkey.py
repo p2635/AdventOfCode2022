4e059455a1258ebf229d65056ea66c46f6fff650
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-11 18:35:29 +0000

    part 1 complete, part 2 is hard

--- day11/Monkey.py
+++ day11/Monkey.py
@@ -1,39 +1,25 @@
 class Monkey:
 
-    def __init__(self, items = [],  operation = "* 11", test = 23):
+    def __init__(self, items,  operation, test, m1, m2):
         self.items = items
         self.operation = operation
-        self.test = test # Assume it's only divisible by
+        self.divi_test = test # Assume it's only division
+        self.target_monkey1 = m1
+        self.target_monkey2 = m2
         self.inspect_count = 0
 
     def inspect(self, item):
-        print(f"\tMonkey inspects item with worry level {item}.")
-        item = eval("item " + self.operation)
-        print(f"\t\tWorry level is {self.operation} to {item}.")
-        item = self.get_bored(item)
-        self.inspect_count += 1
+        item = eval(self.operation)
         return item
-    
-    def inspect_items(self):
-        for item in self.items:
-            self.inspect(item)
 
-    def throw_last_item(self, Monkey):
-        Monkey.items.append(self.items[-1])
-        print(f"\t\tItem with worry level {item} is thrown to a monkey.")
-        self.items.pop()
+    def throw_first_item(self, Monkey):
+        Monkey.items.append(self.items[0])
+        self.items.pop(0)
         return True
 
-    def get_bored(self, item):
-        item = int(item / 3)
-        print(f"\t\tMonkey gets bored with item. Worry level is divided by 3 to {item}.")
+    def get_bored(self, item, divide_by):
+        item = int(item / divide_by)
         return item
 
-    def test(self, item):
-
-        if item % self.test == 0:
-            # self.throw(Monkey)
-            pass
-        else:
-            # self.throw(other monkey)
-            pass
+    def divitest_item(self, item):
+        return item % self.divi_test == 0
--- day11/day11.py
+++ day11/day11.py
@@ -1,18 +1,143 @@
+import re, copy
 from Monkey import Monkey
 
+##############################################################################
+##           Parsing the text file to get a list of monkeys
+##############################################################################
+original_monkeys = []
+
+with open("input/d11.txt", "r") as file:
+    monkey_instructions = [line.strip() for line in file]
+
+# To workaround the problem of the word 'old' conflict with my var name 'item'
+for i in range(len(monkey_instructions)):
+    monkey_instructions[i] = monkey_instructions[i].replace("old", "item")
+
+# For each monkey in the text file, create a new monkey and put it in a monkeys list
+for i in range(0, len(monkey_instructions), 7): # Each monkey is 7 lines in input.txt
+
+    # For log's sake
+    print("== ADDING MONKEY TO A LIST ==")
+    print(monkey_instructions[i])
+
+    # Get all the information
+    items = [int(s) for s in re.findall(r'\d+', monkey_instructions[i + 1])]
+    operation = monkey_instructions[i + 2].split("Operation: new = ", 1)[1]
+    test = int(re.findall(r'\d+', monkey_instructions[i + 3])[0]) # [0] first element to avoid list data type
+    m1 = int(re.findall(r'\d+', monkey_instructions[i + 4])[0])
+    m2 = int(re.findall(r'\d+', monkey_instructions[i + 5])[0])
+    print(items, operation, test, m1, m2, sep="\n")
+
+    # Create a new monkey and add to the list
+    original_monkeys.append(Monkey(items, operation, test, m1, m2))
+
+##############################################################################
+##           Part 1 of the puzzle
+##############################################################################
+print("===== PART 1 BEGINS =====")
+
 rounds = 20
+part1_monkeys = copy.deepcopy(original_monkeys)
+for i in range(rounds):
+    
+    print(f"===== ROUND {i + 1} =====")
+    # For monkey in monkeys, inspect item and throw stuff
+    for i in range(len(part1_monkeys)):
 
-# with open("input/d11.txt", "r") as file:
-#     monkey_instructions = [line.strip() for line in file]
+        current_monkey = part1_monkeys[i]
+        print(f"Monkey {i}:")
 
-m1 = Monkey([79, 98], "* 19")
+        if len(current_monkey.items) == 0:
+            print("\tNo action, the monkey has no items.")
+            continue
 
-m1.inspect_items()
+        print(f"> Current Items: {current_monkey.items}")
+   
+        while len(current_monkey.items) != 0:
+            
+            # Monkey inspects (always the first item, since it keeps throwing items)
+            print(f"\tMonkey inspects item with worry level {current_monkey.items[0]}.")
+            current_monkey.items[0] = current_monkey.inspect(current_monkey.items[0])
+            print(f"\t\tWorry level is {current_monkey.operation} to {current_monkey.items[0]}.")
 
-print(m1.inspect_count)
+            # Monkey gets bored
+            new_value = current_monkey.get_bored(current_monkey.items[0], 3)
+            print(f"\t\tMonkey gets bored with item. Worry level is divided by 3 to {new_value}.")
+            current_monkey.items[0] = new_value
+            
+            # Division test
+            result = current_monkey.divitest_item(current_monkey.items[0])
+            if result:
+                monkey_to_catch = current_monkey.target_monkey1
+            else:
+                monkey_to_catch = current_monkey.target_monkey2 
+            print(f"\t\tCurrent worry level is divisible by {current_monkey.divi_test}: {result}")
+            
+            # Monkey throws for the first item
+            print(f"\t\tItem with worry level {current_monkey.items[0]} is thrown to Monkey {monkey_to_catch}.")
+            current_monkey.throw_first_item(part1_monkeys[monkey_to_catch])
 
-# m2 = Monkey("old * 19", [56, 34])
+            # Monkey keeps track of how many times he inspects
+            current_monkey.inspect_count += 1
 
-# print(m1.items, m2.items)
-# m1.throw(m2)
-# print(m2.items)
+print("===== Inspect counts =====")
+for i in range(len(part1_monkeys)):
+    print(f"* Monkey {i} inspected items {part1_monkeys[i].inspect_count} times.")
+
+##############################################################################
+##           Part 2 of the puzzle
+##           same thing, only difference is more rounds and no dividing
+##############################################################################
+print("===== PART 2 BEGINS =====")
+
+rounds = 10000
+part2_monkeys = copy.deepcopy(original_monkeys)
+
+for i in range(rounds):
+    
+    # print(f"===== ROUND {i + 1} =====")
+    # For monkey in monkeys, inspect item and throw stuff
+    for i in range(len(part2_monkeys)):
+
+        current_monkey = part2_monkeys[i]
+        # print(f"Monkey {i}:")
+
+        if len(current_monkey.items) == 0:
+            # print("\tNo action, the monkey has no items.")
+            continue
+
+        # print(f"> Current Items: {current_monkey.items}")
+   
+        while len(current_monkey.items) != 0:
+            
+            # Monkey inspects (always the first item, since it keeps throwing items)
+            # print(f"\tMonkey inspects item with worry level {current_monkey.items[0]}.")
+            current_monkey.items[0] = current_monkey.inspect(current_monkey.items[0])
+            # print(f"\t\tWorry level is {current_monkey.operation} to {current_monkey.items[0]}.")
+
+            # Monkey gets bored
+            new_value = current_monkey.get_bored(current_monkey.items[0], 4)
+            # print(f"\t\tMonkey gets bored with item. Worry level is divided by 4 to {new_value}.")
+            current_monkey.items[0] = new_value
+            
+            # Division test
+            result = current_monkey.divitest_item(current_monkey.items[0])
+            if result:
+                monkey_to_catch = current_monkey.target_monkey1
+            else:
+                monkey_to_catch = current_monkey.target_monkey2 
+            # print(f"\t\tCurrent worry level is divisible by {current_monkey.divi_test}: {result}")
+            
+            # Monkey throws for the first item
+            # print(f"\t\tItem with worry level {current_monkey.items[0]} is thrown to Monkey {monkey_to_catch}.")
+            current_monkey.throw_first_item(part2_monkeys[monkey_to_catch])
+
+            # Monkey keeps track of how many times he inspects
+            current_monkey.inspect_count += 1
+
+    for i in part2_monkeys:
+        print(i.items)
+
+print("===== Inspect counts =====")
+for i in range(len(part2_monkeys)):
+    print(f"* Monkey {i} inspected items {part2_monkeys[i].inspect_count} times.")
